8d806590cb7517fe34c8f0714468a3797c377ff8
I-No-oNe <novonoam76@gmail.com>
2024-09-26 18:44:19 +0300

    A small fix?

--- src/main/java/net/i_no_am/clickcrystals/addon/mixin/MixinHomeScreen.java
+++ src/main/java/net/i_no_am/clickcrystals/addon/mixin/MixinHomeScreen.java
@@ -8,16 +8,15 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(HomeScreen.class)
 public class MixinHomeScreen implements Global {
 
-
     @Shadow public static boolean OPENED_BEFORE;
 
-    @Inject(remap = false ,method = "mouseClicked", at = @At("HEAD"))
-    private void disableDiscordScreen(double mouseX, double mouseY, int button, CallbackInfoReturnable<Boolean> cir){
+    @Inject(remap = false ,method = "<init>", at = @At("RETURN"))
+    private void disableDiscordScreen(CallbackInfo ci){
         if (Module.get(DiscordScreenDisabler.class).isEnabled()) OPENED_BEFORE = true;
     }
 }
