71f80fdf7504d3b072e5cd6db51effb0c69353b1
I-No-oNe <145749961+I-No-oNe@users.noreply.github.com>
2024-10-21 10:31:11 +0300

    fix

--- src/main/java/net/i_no_am/clickcrystals/addon/modules/Prevent.java
+++ src/main/java/net/i_no_am/clickcrystals/addon/modules/Prevent.java
@@ -10,60 +10,52 @@ import net.minecraft.item.Items;
 import net.minecraft.util.ActionResult;
 
 public class Prevent extends AddonModule {
+
     public Prevent() {
         super("prevent", "Prevent making common mistakes in crystal PvP");
     }
 
     private final SettingSection scGeneral = getGeneralSection();
 
-    public final ModuleSetting<Boolean> disableGlowstoneOnGlowstone = scGeneral.add(createBoolSetting()
-            .name("disable-placing-glowstone-on-glowstone")
-            .description("Disable placing glowstone unless not targeting a glowstone.")
-            .def(true)
-            .build()
-    );
-        public final ModuleSetting<Boolean> disableP = scGeneral.add(createBoolSetting()
-            .name("disable-placing-anchor-on-anchor")
-            .description("Disable placing a respawn anchor on top of another anchor.")
-            .def(true)
-            .build()
-    );
-    public final ModuleSetting<Boolean> disableAnchorOnGlowstone = scGeneral.add(createBoolSetting()
-            .name("disable-placing-anchor-on-glowstone")
-            .description("Disable placing a respawn anchor on glowstone.")
-            .def(true)
-            .build()
-    );
-    public final ModuleSetting<Boolean> disableDoubleAnchorPlacement = scGeneral.add(createBoolSetting()
+    public final ModuleSetting<Boolean> disableAnchorOnAnchor = scGeneral.add(createBoolSetting()
             .name("disable-placing-anchor-on-anchor")
             .description("Disable placing a respawn anchor on top of another anchor.")
             .def(true)
             .build()
     );
     
+    public final ModuleSetting<Boolean> disableAnchorOnGlowstone = scGeneral.add(createBoolSetting()
+            .name("disable-placing-anchor-on-glowstone")
+            .description("Disable placing a respawn anchor on glowstone.")
+            .def(true)
+            .build()
+    );
+    
+    public final ModuleSetting<Boolean> disableGlowstoneOnGlowstone = scGeneral.add(createBoolSetting()
+            .name("disable-placing-glowstone-on-glowstone")
+            .description("Disable placing glowstone unless not targeting another glowstone.")
+            .def(false)
+            .build()
+    );
+
     public ActionResult cannotPlace() {
         if (HotbarUtils.isHolding(Items.RESPAWN_ANCHOR)
-                && disableDoubleAnchorPlacement.getVal()
+                && disableAnchorOnAnchor.getVal()
                 && BlockUtils.isLookingAt(Blocks.RESPAWN_ANCHOR)) {
-            if (BlockUtils.anchorWithCharges(1))
+            if (BlockUtils.anchorWithCharges(1)) {
                 return ActionResult.PASS;
-            else {
+            } else {
                 return ActionResult.FAIL;
             }
-        } else if (HotbarUtils.isHolding(Items.GLOWSTONE)
-                && disableGlowstonePlacement.getVal() &&
-                HotbarUtils.isHoldingEitherHand(Items.GLOWSTONE)
-                && (!BlockUtils.isLookingAt(Blocks.RESPAWN_ANCHOR) || BlockUtils.anchorWithCharges(1))) {
-            return ActionResult.FAIL;
         } else if (HotbarUtils.isHolding(Items.RESPAWN_ANCHOR)
                 && disableAnchorOnGlowstone.getVal()
                 && BlockUtils.isLookingAt(Blocks.GLOWSTONE)) {
             return ActionResult.FAIL;
-        } else if isHolding(Items.GLOWSTONE)
+        } else if (HotbarUtils.isHolding(Items.GLOWSTONE)
                 && disableGlowstoneOnGlowstone.getVal()
                 && BlockUtils.isLookingAt(Blocks.GLOWSTONE)) {
             return ActionResult.FAIL;
+        }
         return ActionResult.PASS;
     }
 }
-
