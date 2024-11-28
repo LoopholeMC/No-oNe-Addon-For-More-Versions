0c5660d9e9cca0715b6dea9a3fa2954aaccee94a
I-No-oNe <novonoam76@gmail.com>
2024-11-28 11:12:58 +0200

    Removed isDev() func

--- src/main/java/net/i_no_am/clickcrystals/addon/modules/data/AddonLModule.java
+++ src/main/java/net/i_no_am/clickcrystals/addon/modules/data/AddonLModule.java
@@ -1,22 +1,11 @@
 package net.i_no_am.clickcrystals.addon.modules.data;
 
-import io.github.itzispyder.clickcrystals.modules.Module;
 import io.github.itzispyder.clickcrystals.modules.modules.ListenerModule;
 import net.i_no_am.clickcrystals.addon.client.AddonCategory;
-import net.i_no_am.clickcrystals.addon.client.Manager;
 
 public class AddonLModule extends ListenerModule {
-    protected final String MOD_ID;
-    protected final boolean isDev;
 
     public AddonLModule(String name, String description) {
         super(name, AddonCategory.ADDON, description);
-        this.MOD_ID = Manager.MOD_ID;
-        this.isDev = Manager.isDev();
-    }
-    //    TODO REMOVE
-    public static boolean isDev() {
-        AddonModule m = Module.get(AddonModule.class);
-        return m.isDev;
     }
 }
--- src/main/java/net/i_no_am/clickcrystals/addon/modules/data/AddonModule.java
+++ src/main/java/net/i_no_am/clickcrystals/addon/modules/data/AddonModule.java
@@ -1,22 +1,11 @@
 package net.i_no_am.clickcrystals.addon.modules.data;
 
-import io.github.itzispyder.clickcrystals.modules.Module;
 import io.github.itzispyder.clickcrystals.modules.modules.DummyModule;
 import net.i_no_am.clickcrystals.addon.client.AddonCategory;
-import net.i_no_am.clickcrystals.addon.client.Manager;
 
 public class AddonModule extends DummyModule {
-    protected final String MOD_ID;
-    protected final boolean isDev;
 
     public AddonModule(String name, String description) {
         super(name, AddonCategory.ADDON, description);
-        this.MOD_ID = Manager.MOD_ID;
-        this.isDev = Manager.isDev();
-    }
-//    TODO REMOVE
-    public static boolean isDev() {
-        AddonModule m = Module.get(AddonModule.class);
-        return m.isDev;
     }
 }
